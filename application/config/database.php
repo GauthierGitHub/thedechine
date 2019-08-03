0d0988999abd00a4f37b957410a9543a2d8845de
gauthier <gauthier.emailpro@gmail.com>
2019-08-03 17:24:10 +0200

    gitignore

--- application/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * Database configuration settings used by PDO.
- */
-
-$config['dsn']      = 'mysql:host=localhost;dbname=thedechine';
-$config['password'] = 'gogo4everGo_Go';
-$config['user']     = 'gogo';
